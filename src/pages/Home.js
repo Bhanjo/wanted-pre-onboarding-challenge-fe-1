f4dfbb5143e6b72e6b66c1cd58c83a9f54cb9e02
Bhanjo <hanjo1515@naver.com>
2022-07-30 19:01:41 +0900

    feat: todo 등록 기능

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,59 @@
-import Axios from '../lib/axsios';
 import styled from 'styled-components';
+import { useRef, useState } from 'react';
 
 // 로그인이 안되어 있다면 로그인 페이지로 이동 기능 구현 필요
 const Home = () => {
-  // todo 패치 예시
-  const fetchTest = async () => {
-    const data = await Axios.get('/todos');
-    console.log(data);
+  const todoId = useRef(1);
+
+  const [todos, setTodos] = useState([
+    {
+      id: 0,
+      title: '제목1',
+      content: '본문1',
+      createdAt: '2022-01-01',
+      updatedAt: '2022-07-01',
+    },
+  ]);
+
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const initState = () => {
+    setTitle('');
+    setContent('');
+  };
+
+  const onChange = e => {
+    setContent(e.target.value);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const todo = {
+      id: todoId.current,
+      title: 'asd',
+      content: content,
+      createdAt: '2020-07-30',
+      updatedAt: '2020-07-30',
+    };
+    todoId.current += 1;
+    setTodos(todos.concat(todo));
+    initState();
   };
 
   return (
     <Container>
-      <TodoContainer>어서오세요!</TodoContainer>
+      <TodoContainer>
+        <form onSubmit={onSubmit}>
+          <input value={content} onChange={onChange} />
+          <button type='submit'>등록</button>
+        </form>
+        <div>
+          {todos.map(todo => (
+            <p key={todo.id}>{todo.content}</p>
+          ))}
+        </div>
+      </TodoContainer>
     </Container>
   );
 };
@@ -23,14 +65,14 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
-  background-color: skyblue;
 `;
 
 const TodoContainer = styled.div`
   min-width: 400px;
   min-height: 500px;
   background-color: blue;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.primary.skywhite};
 `;
 
 export default Home;
