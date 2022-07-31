d45aed6dcacae3c6f51e618cacfca8da3672f2df
Bhanjo <hanjo1515@naver.com>
2022-07-31 21:30:56 +0900

    feat: 회원가입 유효성 검증 구현

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Sign from '../componenets/Sign';
 
@@ -6,10 +6,21 @@ const SignUp = () => {
   const initForm = {
     email: '',
     password: '',
+    rePassword: '',
+  };
+
+  const inputPlaceholder = {
+    email: '이메일',
+    password: '8자리 이상을 입력하세요',
+    rePassword: '비밀번호 재입력',
   };
 
   const [inputs, setInputs] = useState(initForm);
-  const { email, password } = inputs;
+  const { email, password, rePassword } = inputs;
+
+  const [emailValid, setEmailValid] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(false);
+  const [rePasswordVaild, setRePasswordVaild] = useState(false);
 
   const onChange = e => {
     const { name, value } = e.target;
@@ -21,22 +32,62 @@ const SignUp = () => {
 
   const onSubmit = e => {
     e.preventDefault();
-    console.log('email:', email, 'password:', password);
+    alert(`${email} ${password} ${rePassword}`);
     setInputs(initForm);
   };
+
+  const emailPattern =
+    /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+  useEffect(() => {
+    setEmailValid(inputs.email !== '' && inputs.email.match(emailPattern));
+    setPasswordValid(inputs.password.length >= 8);
+    setRePasswordVaild(inputs.password === inputs.rePassword);
+  }, [inputs.email, inputs.password, inputs.rePassword]);
 
   return (
     <Sign title={'회원가입'}>
       <Form onSubmit={onSubmit}>
         <InputContainer>
-          <InputGroup>
-            <label htmlFor='email'>아이디</label>
-            <input name='email' value={email} onChange={onChange} />
-          </InputGroup>
-          <InputGroup>
-            <label htmlFor='password'>비밀번호</label>
-            <input name='password' value={password} onChange={onChange} />
-          </InputGroup>
+          <div>
+            <InputGroup>
+              <label htmlFor='email'>이메일</label>
+              <input
+                type='text'
+                name='email'
+                value={email}
+                placeholder={inputPlaceholder.email}
+                onChange={onChange}
+              />
+            </InputGroup>
+            {!emailValid && inputs.email !== '' && <p>이메일이 올바르지 않습니다</p>}
+          </div>
+          <div>
+            <InputGroup>
+              <label htmlFor='password'>비밀번호</label>
+              <input
+                type='password'
+                name='password'
+                value={password}
+                placeholder={inputPlaceholder.password}
+                onChange={onChange}
+              />
+            </InputGroup>
+            {!passwordValid && inputs.password !== '' && <p>8자리 이상 입력하세요</p>}
+          </div>
+          <div>
+            <InputGroup>
+              <label htmlFor='password'>비밀번호 확인</label>
+              <input
+                type='password'
+                name='rePassword'
+                value={rePassword}
+                placeholder={inputPlaceholder.rePassword}
+                onChange={onChange}
+              />
+            </InputGroup>
+            {!rePasswordVaild && <p>입력하신 비밀번호와 다릅니다.</p>}
+          </div>
         </InputContainer>
         <button type='submit'>회원가입</button>
       </Form>
@@ -73,6 +124,9 @@ const InputContainer = styled.div`
   flex-direction: column;
   gap: 30px 0;
   margin: 30px 20px 0 20px;
+  p {
+    color: red;
+  }
 `;
 
 const InputGroup = styled.div`
@@ -84,11 +138,12 @@ const InputGroup = styled.div`
     color: red;
   }
   input {
-    width: 70%;
+    width: 65%;
     height: 35px;
     padding-left: 10px;
     border-radius: 4px;
     border: 1px solid #d5d4d4;
+    font-size: 18px;
   }
   input:focus {
     outline: 2px solid ${({ theme }) => theme.colors.primary.indigo};
