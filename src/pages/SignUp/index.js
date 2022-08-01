29c381790d5f659bacfab88e3f019f1b6e60fb44
Bhanjo <hanjo1515@naver.com>
2022-08-01 16:52:34 +0900

    feat: 회원가입 api 통신 기능 구현

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Axios from '../../lib/axios';
 import * as Style from './styles';
 import Sign from '../../componenets/Sign';
 import InputBox from '../../componenets/InputBox';
 
 const SignUp = () => {
+  // 리액트 라우터 돔 v6 이후부터는 useHistory 대신 useNavigate 사용
+  const navigate = useNavigate();
   const initForm = {
     email: '',
     password: '',
@@ -14,6 +18,15 @@ const SignUp = () => {
     email: '이메일',
     password: '8자리 이상을 입력하세요',
     rePassword: '비밀번호 재입력',
+  };
+
+  const emailPattern =
+    /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+  const vaildMessages = {
+    email: '이메일이 올바르지 않습니다',
+    password: '8자리 이상 입력하세요',
+    rePassword: '입력하신 비밀번호와 다릅니다.',
   };
 
   const [inputs, setInputs] = useState(initForm);
@@ -31,43 +44,56 @@ const SignUp = () => {
     });
   };
 
-  const onSubmit = e => {
-    e.preventDefault();
-    alert(`${email} ${password} ${rePassword}`);
-    setInputs(initForm);
-  };
-
-  const emailPattern =
-    /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-
-  useEffect(() => {
-    setEmailValid(inputs.email !== '' && inputs.email.match(emailPattern) !== null);
-    setPasswordValid(inputs.password.length >= 8);
-    setRePasswordVaild(inputs.password === inputs.rePassword && inputs.rePassword !== '');
-  }, [inputs.email, inputs.password, inputs.rePassword]);
-
-  const vaildMessages = {
-    email: '이메일이 올바르지 않습니다',
-    password: '8자리 이상 입력하세요',
-    rePassword: '입력하신 비밀번호와 다릅니다.',
-  };
-
   const inputVaild = type => {
     let msg = null;
-    if (type === 'email' && !emailValid && inputs.email) {
+    if (type === 'email' && !emailValid && email) {
       msg = vaildMessages.email;
     }
 
-    if (type === 'password' && !passwordValid && inputs.password) {
+    if (type === 'password' && !passwordValid && password) {
       msg = vaildMessages.password;
     }
 
-    if (type === 'rePassword' && !rePasswordVaild && inputs.rePassword) {
+    if (type === 'rePassword' && !rePasswordVaild && rePassword) {
       msg = vaildMessages.rePassword;
     }
 
     return msg;
   };
+
+  const signupReqest = async () => {
+    try {
+      const data = await Axios.post('/users/create', {
+        email: email,
+        password: password,
+      });
+
+      return data.data;
+    } catch (error) {
+      if (error.request.status === 409) {
+        alert('이미 존재하는 회원입니다.');
+      }
+      console.log('error occured in signupRequst', error);
+    }
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    try {
+      const res = await signupReqest();
+      alert(res.message);
+    } catch (error) {
+      console.log('error occured in onSubmit');
+    }
+    setInputs(initForm);
+    navigate('/');
+  };
+
+  useEffect(() => {
+    setEmailValid(email !== '' && email.match(emailPattern) !== null);
+    setPasswordValid(password.length >= 8);
+    setRePasswordVaild(password === rePassword && rePassword !== '');
+  }, [email, password, rePassword]);
 
   return (
     <Sign title={'회원가입'}>
