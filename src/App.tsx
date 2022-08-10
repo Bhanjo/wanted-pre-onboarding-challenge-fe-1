9e747c7e25b1e2106879224bc64bcfa07fd8b770
Bhanjo <hanjo1515@naver.com>
2022-08-10 20:34:13 +0900

    setting: 스타일 타입 설정, app, index 타입 에러 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import GlobalStyle from './styles/GlobalStyle';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Todo from './pages/Todo';
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('root');
+if (!container) throw new Error('Failed to find root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
